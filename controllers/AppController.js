5d52c26e34d81d400951e90fde452b37c509e551
isaaccleopas <isaaccleopas@gmail.com>
2023-10-01 03:34:13 -0700

    mandatory tasks

---
+++ controllers/AppController.js
@@ -0,0 +1,26 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+/**
+ * AppController class
+ */
+class AppController {
+  
+  static getStatus(request, response) {
+    response.statusCode = 200;
+    response.send({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static async getStats(request, response) {
+    response.statusCode = 200;
+    response.send({
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    });
+  }
+}
+
+export default AppController;
